58d3a7426ecf21f1553e660bb6a19ff489007130
Bart Massey <bart.massey@gmail.com>
2023-04-23 21:48:22 -0700

    added WAV output

--- tone.py
+++ tone.py
@@ -15,13 +15,25 @@ import sounddevice as sd
 # horribly.
 BUFFER_SIZE = 2048
 
-# Read from a 16-bit WAV file. Returns 
+# Read from a 16-bit WAV file. Returns the sample rate in
+# samples per second, and the samples as a numpy array of
+# IEEE 64-bit floats. The array will be 1D for mono data,
+# and will be a 2D array of 2-element frames for stereo
+# data.
 def read_wav(filename):
     rate, data = io.wavfile.read(filename)
     assert data.dtype == np.int16
     data = data.astype(np.float64)
     data /= 32768
     return rate, data
+
+# Write to a 16-bit WAV file. Data is in the same
+# format produced by read_wav().
+def write_wav(filename, rate, data):
+    assert data.dtype == np.float64
+    data *= 32767
+    data = data.astype(np.int16)
+    io.wavfile.write(filename, rate, data)
 
 # Play a tone on the computer.
 def play(rate, wav):
@@ -100,6 +112,10 @@ argp.add_argument(
     type=int,
     default=0,
 )
+argp.add_argument(
+    "--out",
+    help="write to WAV file instead of playing",
+)
 argp.add_argument("wav", help="audio file")
 args = argp.parse_args()
 
@@ -163,5 +179,8 @@ gain = knob(args.volume, offset=9.0)
 result = gain * np.sum(filtered, axis=0)
 out_data = np.transpose(result)
 
-# Play result.
-play(rate, out_data)
+# Render or play result.
+if args.out:
+    write_wav(args.out, rate, out_data)
+else:
+    play(rate, out_data)
